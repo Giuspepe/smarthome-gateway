7409e4043c84892f600734dd6ae8debf22f35d27
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2019-03-15 12:47:46 +0100

    worked on api and added some tests

--- api.py
+++ api.py
@@ -1,19 +1,26 @@
-# TODO: remove marshal? if not implement it in all functions that return a device
+# TODO: more endpoints? /devices/huelights, /devices/sensors, /devices/whatever ?
 
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, fields, marshal
 import shelve
 import functools
+import copy
+import json
+from control_hue_lights import set_state
 
 app = Flask(__name__)
 api = Api(app)
 
+api.app.config['DATABASE'] = 'database_shelve'
 
-def db_shelve_decorator(func):
+device_attributes = ['device_id', 'device_name', 'device_type', 'device_controller_address', 'device_data']
+
+def shelve_db_decorator(func):
     @functools.wraps(func)
     def inner(*args, **kwargs):
-        global db_shelve  # global is necessary, otherwise func can't see db_shelve
-        with shelve.open('database_shelve', writeback=True) as db_shelve:  # opens and closes shelve automatically
+        global shelve_db  # global is necessary, otherwise func can't see shelve_db
+        # opens and closes shelve database automatically even if an exception is raised
+        with shelve.open(api.app.config['DATABASE'], writeback=True) as shelve_db:
             rv = func(*args, **kwargs)
         return rv
     return inner
@@ -22,27 +29,28 @@ def db_shelve_decorator(func):
 class DeviceListAPI(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('device_id', type=str, required=True, help='No device_id provided', location='json')
+        self.reqparse.add_argument('device_id', type=str, required=True,
+                                   help='No/invalid device_id provided', location='json')
         self.reqparse.add_argument('device_name', type=str, required=True,
-                                   help='No device_name provided', location='json')
+                                   help='No/invalid device_name provided', location='json')
         self.reqparse.add_argument('device_type', type=str, required=True,
-                                   help='No device_type provided', location='json')
+                                   help='No/invalid device_type provided', location='json')
         self.reqparse.add_argument('device_controller_address', type=str, required=True,
-                                   help='No device_controller_address provided', location='json')
-        self.reqparse.add_argument('device_data', type=str, required=True,
-                                   help='No device_data provided', location='json')
+                                   help='No/invalid device_controller_address provided', location='json')
+        self.reqparse.add_argument('device_data', type=dict, required=True,
+                                   help='No/invalid device_data provided', location='json')
         super(DeviceListAPI, self).__init__()
 
-    @db_shelve_decorator
+    @shelve_db_decorator
     def get(self):
-        return db_shelve['devices'], 200
+        return shelve_db['devices'], 200
 
-    @db_shelve_decorator
+    @shelve_db_decorator
     def post(self):
         args = self.reqparse.parse_args()
 
-        if args['device_id'] in list(db_shelve.keys()):
-            return f'Device with device_id {args["device_id}"]} already exists', 409
+        if [device for device in shelve_db['devices'] if device['device_id'] == args['device_id']]:
+            return f'Device with device_id "{args["device_id"]}" already exists', 409
 
         new_device = {
             'device_id': args['device_id'],
@@ -51,15 +59,17 @@ class DeviceListAPI(Resource):
             'device_controller_address': args['device_controller_address'],
             'device_data': args['device_data']
         }
-        db_shelve['devices'].append(new_device)
+        shelve_db['devices'].append(new_device)
         return marshal(new_device, device_fields), 200
 
 
 device_fields = {
-    'uri': fields.Url('device'),
+    'uri': fields.Url('device', absolute=True),
+    'device_id': fields.String,
     'device_name': fields.String,
     'device_type': fields.String,
-    'device_data': fields.String
+    'device_controller_address': fields.String,
+    'device_data': fields.Raw
 }
 
 
@@ -70,53 +80,95 @@ class DeviceAPI(Resource):
         self.reqparse.add_argument('device_name', type=str, location='json')
         self.reqparse.add_argument('device_type', type=str, location='json')
         self.reqparse.add_argument('device_controller_address', type=str, location='json')
-        self.reqparse.add_argument('device_data', type=str, location='json')
+        self.reqparse.add_argument('device_data', type=dict, location='json')
         super(DeviceAPI, self).__init__()
 
-    @db_shelve_decorator
+    @shelve_db_decorator
     def get(self, device_id):
-        device = [device for device in db_shelve['devices'] if device['device_id'] == device_id]
+        device = [device for device in shelve_db['devices'] if device['device_id'] == device_id]
         if len(device) == 0:
             # device not found
-            return f"Couldn't find device with id {device_id}", 404
+            return f'There is no device with device_id "{device_id}"', 404
         if len(device) == 1:
             # device found
-            return device[0], 200
-        # TODO: remove this? it's not possible to add devices with an already existing device_id
+            return marshal(device[0], device_fields), 200
         if len(device) > 1:
-            return f"Found {len(device)} devices with the same device_id", 500
+            return f'Found {len(device)} devices with the same device_id', 500
 
-    @db_shelve_decorator
+    @shelve_db_decorator
     def patch(self, device_id):
         # get copy of device you want to edit
-        device = [device for device in enumerate(db_shelve['devices']) if device[1]['device_id'] == device_id]
+        device = [device for device in enumerate(shelve_db['devices']) if device[1]['device_id'] == device_id]
         device_index = device[0][0]
-        device = device[0][1]
+        device = copy.deepcopy(device[0][1])
 
-        # edit copy
+        # check input
         args = self.reqparse.parse_args()
-        for k, v in args.items():
-            if v is not None:
-                device[k] = v
+        # TODO: don't need this because regparse ensures that you can only enter device_id, device_name, device_type,...
+        if not set(list(args.keys())).issubset(device_attributes):
+            invalid_attributes = set(device_attributes) - set(list(args.keys()))
+            return f'You provided invalid attributes: {invalid_attributes}', 400
 
-        # write edited copy back
-        db_shelve['devices'][device_index] = device
+        # 0. close db
+        for key, val in args.items():
+            if val is not None:
+                if key != 'device_data':
+                    # change 'device_id', 'device_name', 'device_type', 'device_controller_address'
+                    device[key] = val
+                else:
+                    # change hardware data (e.g. color of a light)
+                    if 'Hue' in device['device_type']:
+                        # TODO: call hue api
+                        # 1. call respective API with data to be changed
+                        print('set state')
+                        print(key, val)
+                        #set_state(device_id, {key: val})
+                        pass
+                    elif 'another device type' in device['device_type']:
+                        # TODO: to be implemented
+                        # 0. close db?
+                        # 1. call respective API with data to be changed
+                        # 2. call respective API to get new device data
+                        # 3. open db again?
+                        # 4. write changed device data back to device
+                        pass
+                    elif 'custom device' in device['device_type']:
+                        # TODO: to be implemented
+                        # 0. close db?
+                        # 1. call respective API with data to be changed
+                        # 2. call respective API to get new device data
+                        # 3. open db again?
+                        # 4. write changed device data back to device
+                        pass
+                    else:
+                        return f'Device type "{device["device_type"]}" not implemented yet', 400
 
+        # 2. call respective API to get new device data
+        # if 'Hue' in device['device_type']:
+        #     new_device_data = get(/lights/device_id) oder:
+        #     new_device_data = get_light_info(device_id)
+        #     new_device_data = new_device_data['state']
+        # elif 'another device type' in device['device_type']:
+        #     new_device_data = get(...)
+        # ...
+
+        # 3. open db again
+        # 4. device['device_data'] = new_device_data
+
+        # write back edited copy
+        shelve_db['devices'][device_index] = device
         return marshal(device, device_fields), 200
-        # TODO: bei Verändern einer Geräteeigenschaft zum Zweck der Steuerung muss nach dem Schließen des shelves
-        # TODO: diese Veränderung dem Steuerprogramm mitgeteilt werden damit die Änderung umgesetzt werden kann
-        # TODO: (also z.B. Lampe anschalten). Sonst müsste das Steuerprogramm die shelve immer pollen auf Änderungen
 
-    @db_shelve_decorator
+    @shelve_db_decorator
     def delete(self, device_id):
-        device = [device for device in enumerate(db_shelve['devices']) if device[1]['device_id'] == device_id]
+        device = [device for device in enumerate(shelve_db['devices']) if device[1]['device_id'] == device_id]
         if device:
             device_index = device[0][0]
             device = device[0][1]
-            del db_shelve['devices'][device_index]
+            del shelve_db['devices'][device_index]
             return f'Deleted device {device}', 200
         else:
-            return f'There is no device with device_id {device_id}', 404
+            return f'There is no device with device_id "{device_id}"', 404
 
 
 api.add_resource(DeviceListAPI, '/devices', endpoint='devices')
---
+++ test_api.py
@@ -0,0 +1,115 @@
+import pytest
+import json
+import shelve
+import api
+import copy
+import os
+
+valid_mock_device_1 = {
+        "device_id": "1234",
+        "device_name": "Hue color lamp 1",
+        "device_type": "Extended color light (Hue)",
+        "device_controller_address": "http://192.168.130.214/api/tAC901PfVfQMmNvrhc120uZeBa-Va8SNjc4vhtyh/1",
+        "device_data": {
+            "on": False,
+            "bri": 254,
+            "hue": 47104,
+            "sat": 254,
+            "effect": "none",
+            "xy": [
+                0.1532,
+                0.0475
+            ],
+            "ct": 153,
+            "alert": "select",
+            "colormode": "xy",
+            "mode": "homeautomation",
+            "reachable": True
+        }
+    }
+valid_mock_device_2 = copy.deepcopy(valid_mock_device_1)
+valid_mock_device_2['device_id'] = '5678'
+
+
+@pytest.fixture
+def client():
+    global shelve_db
+    api.app.config['DATABASE'] = 'test_database'
+    api.app.config['TESTING'] = True
+    client = api.app.test_client()
+
+    with api.app.app_context():
+        shelve_db = shelve.open(api.app.config['DATABASE'])
+        shelve_db['devices'] = [valid_mock_device_1, valid_mock_device_2]
+
+    yield client
+
+    shelve_db.close()
+    os.remove('test_database.dat')
+    os.remove('test_database.dir')
+    os.remove('test_database.bak')
+
+
+class TestDeviceListAPI:
+    def test_get(self, client):
+        # get device list
+        rv = client.get('/devices')
+        rv = json.loads(rv.data)
+
+        assert rv == [valid_mock_device_1, valid_mock_device_2]
+
+    def test_post_add_new_device(self, client):
+        new_device = copy.deepcopy(valid_mock_device_1)
+        new_device['device_id'] = 'new_id'
+        rv = client.post('/devices', json=new_device)
+        rv = json.loads(rv.data)
+
+        assert '/devices/new_id' in rv['uri']
+        del rv['uri']
+        assert rv == new_device
+
+    def test_post_add_device_with_existing_id(self, client):
+        rv = client.post('/devices', json=valid_mock_device_1)
+        rv = json.loads(rv.data)
+
+        assert rv == f'Device with device_id \"{valid_mock_device_1["device_id"]}\" already exists'
+
+
+class TestDeviceAPI:
+    def test_get(self, client):
+        # get device from existing device_id
+        valid_device_id = valid_mock_device_1['device_id']
+        rv = client.get(f'/devices/{valid_device_id}')
+        rv = json.loads(rv.data)
+        assert f'/devices/{valid_device_id}' in rv['uri']
+        del rv['uri']
+        assert rv == valid_mock_device_1
+
+        # get device that doesn't exist
+        rv = client.get('/devices/invalid_device_id')
+        rv = json.loads(rv.data)
+        assert f'There is no device with device_id "invalid_device_id"' in rv
+
+    def test_patch(self, client):
+        # TODO: write this test
+        assert 1 == 2
+
+    def test_delete(self, client):
+        # delete device
+        device = copy.deepcopy(valid_mock_device_1)
+        rv = client.delete(f'/devices/{device["device_id"]}')
+        rv = rv.data
+        assert 'Deleted device' in str(rv)
+        devices = client.get('/devices')
+        devices = json.loads(devices.data)
+        assert devices == [valid_mock_device_2]
+
+        # delete device that doesn't exist
+        device = copy.deepcopy(valid_mock_device_1)
+        rv = client.delete(f'/devices/{device["device_id"]}')
+        rv = str(rv.data)
+        assert f'There is no device with device_id' in rv
+
+
+if __name__ == '__main__':
+    pytest.main()
